77ab9ea60c6162add4e78a1b4fd13963cb9069f8
Marko19907 <marko.stancic23@gmail.com>
2023-12-03 15:49:44 +0100

    Made sure that the program can be stopped easily by excluding the tray fly-out menu from the movement

--- src/mover.rs
+++ src/mover.rs
@@ -56,6 +56,7 @@ lazy_static! {
         [
             "#32768", // OLEDShift right click menu
             "NarratorHelperWindow", // A small circle/line, more info here: https://github.com/Marko19907/OLEDShift/issues/12
+            "TopLevelWindowForOverflowXamlIsland", // "Hidden Icon Menu", the flyout menu that appears when you click the arrow on the taskbar
         ].iter().cloned().collect()
     };
 }
